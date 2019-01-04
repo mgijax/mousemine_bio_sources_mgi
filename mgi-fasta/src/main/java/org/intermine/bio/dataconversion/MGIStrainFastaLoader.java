0d7690ad22eefbbfc2e840f551e6b44d5ec302bf
julie-sullivan <julie@flymine.org>
2019-01-04 11:13:18 +0000

    update to latest biojava library version

--- mgi-fasta/src/main/java/org/intermine/bio/dataconversion/MGIStrainFastaLoader.java
+++ mgi-fasta/src/main/java/org/intermine/bio/dataconversion/MGIStrainFastaLoader.java
@@ -1,7 +1,6 @@
 package org.intermine.bio.dataconversion;
 
-import org.biojava.bio.Annotation;
-import org.biojava.bio.seq.Sequence;
+import org.biojava.nbio.core.sequence.template.Sequence;
 import org.intermine.bio.dataconversion.FastaLoaderTask;
 
 /**
@@ -14,10 +13,29 @@ public class MGIStrainFastaLoader extends FastaLoaderTask
      */
     @Override
     protected String getIdentifier(Sequence bioJavaSequence) {
-        Annotation anno = bioJavaSequence.getAnnotation();
-        String header = anno.getProperty("description_line").toString();
+    //    Annotation anno = bioJavaSequence.getAnnotation();
+    //    String header = anno.getProperty("description_line").toString();
 	// 6|A/J dna_rm:chromosome chromosome:A_J_v1:6:1:148608796:1 REF
 	// 6|129S1/SvImJ dna_rm:chromosome chromosome:129S1_SvImJ_v1:6:1:154738780:1 REF
-	return header.split("\\s")[0];
+	//return header.split("\\s")[0];
+
+        String header = bioJavaSequence.getAccession().getID();
+        String name = null;
+
+        if (header.contains("|")) {
+            String[] bits = header.split("\\|");
+            if (bits.length < 2) {
+                return null;
+            }
+            name = bits[1];
+        }
+
+        if (name.contains(" ")) {
+            String[] bits = name.split(" ");
+            name = bits[0];
+        }
+
+        return name;
+
     }
 }
